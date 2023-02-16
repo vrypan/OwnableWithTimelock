33702bc265156685658c603213124f576c63aa48
Panayotis Vryonis <vrypan@gmail.comm>
2023-02-16 22:50:06 +0200

    Removed unused code from SimpleToken.sol

--- contracts/SimpleToken.sol
+++ contracts/SimpleToken.sol
@@ -9,11 +9,4 @@ contract SimpleToken is ERC20, OwnableWithTimelock {
     constructor(uint256 initialSupply) ERC20("SimpleToken", "SIMPLE") {
         _mint(msg.sender, initialSupply);
     }
-
-    /*  mint() can only be used by the contract's owner.
-        We will use it to test onwership transfer.
-    */
-    function mint(address account, uint amount) public onlyOwner {
-        _mint(account, amount);
-    }
 }
